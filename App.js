4a2349ae9e74264afc1b4ef904bce48e7a141377
Sohaib Salman <sohaibsalman10@gmail.com>
2021-04-07 01:40:26 +0500

    Create Button component

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Button, View } from "react-native";
+import AppButton from "./app/components/AppButton";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      <AppButton title="Login" onPress={() => console.log("tapped")} />
+    </View>
+  );
 }
