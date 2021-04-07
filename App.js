37498fe25ea8f8540bb29642bab4785dec60ba9f
Sohaib Salman <sohaibsalman10@gmail.com>
2021-04-07 15:02:27 +0500

    Complete styling section

--- App.js
+++ App.js
@@ -2,13 +2,11 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Button, View } from "react-native";
 import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <AppButton title="Login" onPress={() => console.log("tapped")} />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
