37ad8f2d43efdd8df00c3871227ab4726c783f73
Sohaib Salman <sohaibsalman10@gmail.com>
2021-04-08 11:45:52 +0500

    Create ListingsScreen to show cards of items

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <AccountScreen />;
+  return <ListingsScreen />;
 }
